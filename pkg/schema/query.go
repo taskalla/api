e5bb9871a97cb07c54b1e86d8b335a65dd96c89e
Caleb Denio <cjdenio44@gmail.com>
2020-10-31 17:56:45 -0400

    ✨ Finish viewer field

--- pkg/schema/query.go
+++ pkg/schema/query.go
@@ -1,7 +1,10 @@
 package schema
 
 import (
+	"errors"
+
 	"github.com/graphql-go/graphql"
+	"github.com/taskalla/api/pkg/models/token"
 	"github.com/taskalla/api/pkg/models/user"
 )
 
@@ -11,8 +14,19 @@ var RootQuery = graphql.NewObject(graphql.ObjectConfig{
 		"viewer": &graphql.Field{
 			Type: user.UserObj,
 			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				root := p.Info.RootValue.(map[string]interface{})
+				token, ok := root["token"].(*token.Token)
+				if !ok || token == nil {
+					return nil, errors.New("invalid auth")
+				}
+
+				db_user, err := user.GetUserById(token.UserID)
+				if err != nil {
+					return nil, errors.New("invalid auth")
+				}
+
 				// TODO
-				return nil, nil
+				return db_user, nil
 			},
 		},
 	},
