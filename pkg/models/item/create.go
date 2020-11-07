f7ed0d79729089cf77705e3cf048ed5178a0c6ed
Caleb Denio <cjdenio44@gmail.com>
2020-11-07 15:45:48 -0500

    ✨ Create item mutation

--- pkg/models/item/create.go
+++ pkg/models/item/create.go
@@ -6,28 +6,40 @@ import (
 	"github.com/graphql-go/graphql"
 	"github.com/taskalla/api/pkg/db"
 	"github.com/taskalla/api/pkg/tokenutils"
+	"github.com/taskalla/api/pkg/utils"
 )
 
 var CreateItemMutation = &graphql.Field{
 	Name: "createItem",
 	Type: ItemObj,
+	Args: graphql.FieldConfigArgument{
+		"input": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(CreateItemInput),
+		},
+	},
 	Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 		t, err := tokenutils.ExtractToken(p)
 		if err != nil {
 			return nil, err
 		}
 
-		return Item{
-			Description: t.UserID,
-		}, nil
+		input := p.Args["input"].(map[string]interface{})
+
+		item, err := CreateItem(&CreateItemParams{
+			Description: input["description"].(string),
+			UserID:      t.UserID,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		return item, nil
 	},
 }
 
 var CreateItemInput = graphql.NewInputObject(graphql.InputObjectConfig{
+	Name: "CreateItemInput",
 	Fields: graphql.InputObjectConfigFieldMap{
-		"title": &graphql.InputObjectFieldConfig{
-			Type: graphql.String,
-		},
 		"description": &graphql.InputObjectFieldConfig{
 			Type: graphql.NewNonNull(graphql.String),
 		},
@@ -36,12 +48,19 @@ var CreateItemInput = graphql.NewInputObject(graphql.InputObjectConfig{
 
 type CreateItemParams struct {
 	UserID      string
-	Title       string
 	Description string
 }
 
-func CreateItem(params CreateItemParams) (*Item, error) {
-	db.DB.Exec(context.Background(), "INSERT INTO items (id, title, item_description, user_id) VALUES ()")
+func CreateItem(params *CreateItemParams) (*Item, error) {
+	id := utils.GenerateUUID()
+	_, err := db.DB.Exec(context.Background(), "INSERT INTO items (id, item_description, user_id) VALUES ($1, $2, $3)", id, params.Description, params.UserID)
+	if err != nil {
+		return nil, err
+	}
 
-	return nil, nil
+	return &Item{
+		Description: params.Description,
+		UserID:      params.UserID,
+		ID:          id,
+	}, nil
 }
