92ca1fcce490c3966e391f53995e85393a26bc46
Caleb Denio <cjdenio44@gmail.com>
2020-11-17 19:20:53 -0500

    ✨ Add filtering on viewer.items query

--- pkg/models/item/get.go
+++ pkg/models/item/get.go
@@ -20,14 +20,23 @@ func UserItemsResolver(p graphql.ResolveParams, o paginate.ConnectionOptions) ([
 		after = &a
 	}
 
+	filter_struct := ItemFilter{}
+	if filter_map, ok := p.Args["filter"].(map[string]interface{}); ok {
+		// There's a filter object
+		if filter_done, ok := filter_map["done"].(bool); ok {
+			filter_struct.Done = &filter_done
+		}
+	}
+
 	rows, err := db.DB.Query(context.Background(), `
 		WITH items AS (
 			SELECT ROW_NUMBER() OVER (ORDER BY created_at DESC) AS cursor, id, item_description, user_id, done, created_at FROM items
 			WHERE user_id = $1
+			AND done = coalesce($2, done)
 		) SELECT * FROM items
-		WHERE cursor > coalesce($2, 0)
-		LIMIT $3
-	`, t.UserID, after, o.First+1)
+		WHERE cursor > coalesce($3, 0)
+		LIMIT $4
+	`, t.UserID, filter_struct.Done, after, o.First+1)
 	if err != nil {
 		return nil, nil, err
 	}
