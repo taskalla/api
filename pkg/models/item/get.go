ac8d0944e5c6bb56b0c74049053c379e3e403008
Caleb Denio <cjdenio44@gmail.com>
2020-11-17 19:01:04 -0500

    🔥 Remove unnecessary GetUserItems function

--- pkg/models/item/get.go
+++ pkg/models/item/get.go
@@ -9,27 +9,6 @@ import (
 	"github.com/taskalla/api/pkg/tokenutils"
 )
 
-func GetUserItems(user string, count, page int, filter ItemFilter) ([]*Item, error) {
-	rows, err := db.DB.Query(context.Background(), "SELECT id, item_description, user_id, done, created_at FROM items WHERE user_id = $1 AND done = coalesce($2, done) ORDER BY id ASC LIMIT $3 OFFSET $4", user, filter.Done, count, (page-1)*count)
-	if err != nil {
-		return nil, err
-	}
-
-	items := []*Item{}
-
-	for rows.Next() {
-		item := &Item{}
-		err := rows.Scan(&item.ID, &item.Description, &item.UserID, &item.Done, &item.CreatedAt)
-		if err != nil {
-			return nil, err
-		}
-
-		items = append(items, item)
-	}
-
-	return items, nil
-}
-
 func UserItemsResolver(p graphql.ResolveParams, o paginate.ConnectionOptions) ([]paginate.Edge, *paginate.PageInfo, error) {
 	t, err := tokenutils.ExtractToken(p)
 	if err != nil {
